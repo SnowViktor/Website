bfa9395f980a42310f55f51ebf8a885e49539091
SnowViktor <Snow-Forest@outlook.com>
2024-07-31 23:50:31 +0800

    optimization, remove ViewTransitions

--- public/fonts/original/minify-font.js
+++ public/fonts/original/minify-font.js
@@ -1,26 +1,26 @@
 import Fontmin from "fontmin";
-import * as fs from "fs";
-
-const text = fs.readFile('public/fonts/original/jf-openhuninn-2.0.txt', 'utf-8', function(err, data) {
-    if (err) throw err;
-    return data;
-});
+import fs from "fs";
+import path from "path";
 
 const fontmin = new Fontmin()
-    .src('public/fonts/original/jf-openhuninn-2.0.ttf')
+    .src('public/fonts/original/*.ttf')
     .use(Fontmin.glyph({
-        text: text,
+        text: 'SnowViktor關於工具文章首頁字數計算單詞字符密碼產生已複製！測試0123456789',
         hinting: false
     }))
     .use(Fontmin.ttf2woff2())
     .dest('public/fonts');
 
-fontmin.run(function (err, files) {
+fontmin.run(err => {
     if (err) throw err;
-    console.log(files[0]);
 
-    fs.rm('public/fonts/jf-openhuninn-2.0.ttf', (err) => {
+    fs.readdir('public/fonts', (err, files) => {
         if (err) throw err;
-        console.log('public/fonts/jf-openhuninn-2.0.ttf was deleted');
+        files.forEach(file => {
+            if (path.extname(file) === '.ttf') {
+                const filePath = path.join('public/fonts', file);
+                fs.unlink(filePath, err => {if (err) throw err;});
+            }
+        });
     });
 });
