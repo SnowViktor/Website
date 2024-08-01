8e013aa200e9d8548c3f1be6867f61ff9af3321d
SnowViktor <Snow-Forest@outlook.com>
2024-08-02 00:33:27 +0800

    fix, og, optimization

--- public/fonts/original/minify-font.js
+++ public/fonts/original/minify-font.js
@@ -3,9 +3,9 @@ import fs from "fs";
 import path from "path";
 
 const fontmin = new Fontmin()
-    .src('public/fonts/original/*.ttf')
+    .src('public/fonts/original/jf-openhuninn-2.0.ttf')
     .use(Fontmin.glyph({
-        text: 'SnowViktor關於工具文章首頁字數計算單詞字符密碼產生已複製！測試0123456789',
+        text: "首頁SnowViktor文章測試工具字數計算密碼產生單詞字符0123456789已複製！關於SnowViktor's GitHubWebsite GitHub",
         hinting: false
     }))
     .use(Fontmin.ttf2woff2())
