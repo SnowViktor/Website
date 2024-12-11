3a8304cd5441c4c6b7d903d3a4042e14be838b9f
SnowViktor <Snow-Forest@outlook.com>
2024-12-11 22:43:47 +0800

    @playform/compress

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,12 @@ import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel';
 import sitemap from '@astrojs/sitemap';
 
+import playformCompress from '@playform/compress';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://snow-viktor.vercel.app',
   output: 'server',
   adapter: vercel(),
-  integrations: [sitemap()]
+  integrations: [sitemap(), playformCompress()]
 });
