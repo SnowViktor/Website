845b048f69c096a0983d458def74b013ddfb1c7b
SnowViktor <Snow-Forest@outlook.com>
2025-02-06 01:08:16 +0800

    feat: update article structure and content organization; remove outdated articles and adjust dependencies

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,17 +1,13 @@
 import { defineCollection, z } from "astro:content";
 import { glob } from "astro/loaders";
 
-const articles = defineCollection({
-    loader: glob({ pattern: "**\/[^_]*.md", base: "./src/data/articles" }),
+const ContactAndCommunicationBookDiaries = defineCollection({
+    loader: glob({ pattern: "**\/[^_]*.md", base: "./src/data/contact-and-communication-book-diaries" }),
     schema: z.object({
         title: z.string(),
         description: z.string(),
-        pubDate: z.union([
-            z.date(),
-            z.number(),
-            z.string().optional().nullable()
-        ])
+        pubDate: z.date()
     })
 });
 
-export const collections = { articles };
+export const collections = { ContactAndCommunicationBookDiaries };
