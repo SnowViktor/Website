b626354fcd61726364abaabf6feb0ab0d943f5c8
SnowViktor <Snow-Forest@outlook.com>
2025-02-16 17:54:20 +0800

    feat: refactor diary layout and update dependencies; improve article titles and descriptions

--- src/content.config.ts
+++ src/content.config.ts
@@ -2,11 +2,10 @@ import { defineCollection, z } from "astro:content";
 import { glob } from "astro/loaders";
 
 const ContactAndCommunicationBookDiaries = defineCollection({
-    loader: glob({ pattern: "**\/[^_]*.md", base: "./src/data/contact-and-communication-book-diaries" }),
+    loader: glob({ pattern: "**/[^_]*.md", base: "./src/data/contact-and-communication-book-diaries" }),
     schema: z.object({
-        title: z.string(),
-        description: z.string(),
-        pubDate: z.string().date()
+        title: z.string().nullable(),
+        description: z.string()
     })
 });
 
