d8c13bb843d477884679fc110c1b286a071a841e
SnowViktor <Snow-Forest@outlook.com>
2025-03-01 00:41:24 +0800

    feat: add Prose collection and update article display; enhance layout and styling for better readability

--- src/content.config.ts
+++ src/content.config.ts
@@ -10,4 +10,13 @@ const ContactAndCommunicationBookDiaries = defineCollection({
     })
 });
 
-export const collections = { ContactAndCommunicationBookDiaries };
+const Prose = defineCollection({
+    loader: glob({ pattern: "**/[^_]*.md", base: "./src/data/prose" }),
+    schema: z.object({
+        title: z.string(),
+        description: z.string(),
+        pubDate: z.date()
+    })
+});
+
+export const collections = { ContactAndCommunicationBookDiaries, Prose };
