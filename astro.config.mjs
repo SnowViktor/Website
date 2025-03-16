a99b0d3a7bbccb71874a3804a98122e5f97399a4
SnowViktor <Snow-Forest@outlook.com>
2025-03-16 22:03:12 +0800

    feat: update Astro and add math rendering support; create new diary entries and enhance article layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
+
 import vercel from '@astrojs/vercel';
 import sitemap from '@astrojs/sitemap';
 
@@ -11,5 +14,9 @@ export default defineConfig({
   site: 'https://snow-viktor.vercel.app',
   output: 'server',
   adapter: vercel(),
-  integrations: [sitemap(), playformCompress()]
+  integrations: [sitemap(), playformCompress()],
+  markdown: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [[rehypeKatex, { output: 'mathml' }]]
+  }
 });
