6d7d73c49cae2b047ea4e96578c9b56eefee8306
SnowViktor <Snow-Forest@outlook.com>
2025-06-21 18:19:35 +0800

    Add new articles and remove outdated diary entries

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,42 +1,15 @@
 import { defineCollection, z, type CollectionKey } from "astro:content";
 import { glob } from "astro/loaders";
 
-const ContactAndCommunicationBookDiaries = defineCollection({
-  loader: glob({
-    pattern: "**/[^_]*.md",
-    base: "./src/data/contact-and-communication-book-diaries",
-  }),
-  schema: z.object({
-    title: z.string().nullable(),
-    description: z.string(),
-    pubDate: z.date(),
-  }),
-});
-const Prose = defineCollection({
-  loader: glob({ pattern: "**/[^_]*.md", base: "./src/data/prose" }),
+const Articles = defineCollection({
+  loader: glob({ pattern: "**/[^_]*.md", base: "./src/data/articles" }),
   schema: z.object({
     title: z.string(),
     description: z.string(),
     pubDate: z.date(),
-  }),
-});
-const ReadingReflection = defineCollection({
-  loader: glob({
-    pattern: "**/[^_]*.md",
-    base: "./src/data/reading-reflection",
-  }),
-  schema: z.object({
-    title: z.string(),
-    description: z.string(),
-    pubDate: z.date(),
-    author: z.string(),
   }),
 });
 
-export const collections = {
-  ContactAndCommunicationBookDiaries,
-  Prose,
-  ReadingReflection,
-};
+export const collections = { Articles };
 export const collectionKey = Object.keys(collections) as CollectionKey[];
-export const collectionDisplayName = ["聯絡簿日記", "散文", "閱讀心得"];
+export const collectionDisplayName = ["文章"];
