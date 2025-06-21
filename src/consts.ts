0db49dcc0f12c6a93ad4c8914d58334b0491a36d
SnowViktor <Snow-Forest@outlook.com>
2025-06-21 18:30:26 +0800

    feat: add site constants and update articles collection schema

---
+++ src/consts.ts
@@ -0,0 +1,14 @@
+export const SITE_TITLE = "SnowViktor";
+export const SITE_DESCRIPTION =
+  "【SnowViktor's Personal Website｜彬彬的個人網站】";
+export const SITE_AUTHOR = "SnowViktor｜彬彬";
+
+export const CATEGORIES = [
+  { name: "聯絡簿日記", id: "contact-and-communication-book-diary" },
+  { name: "閱讀心得", id: "reading-reflection" },
+  { name: "散文", id: "prose" },
+  { name: "新詩", id: "free-verse" },
+  { name: "學校", id: "school" },
+  { name: "漫畫", id: "comic" },
+  { name: "法律", id: "law" },
+];
--- src/content.config.ts
+++ src/content.config.ts
@@ -1,15 +1,36 @@
-import { defineCollection, z, type CollectionKey } from "astro:content";
 import { glob } from "astro/loaders";
+import {
+  defineCollection,
+  z,
+  type CollectionKey,
+  type SchemaContext,
+} from "astro:content";
+import { SITE_AUTHOR, CATEGORIES } from "./consts.ts";
 
-const Articles = defineCollection({
-  loader: glob({ pattern: "**/[^_]*.md", base: "./src/data/articles" }),
-  schema: z.object({
-    title: z.string(),
-    description: z.string(),
-    pubDate: z.date(),
-  }),
+const imageSchema = ({ image }: SchemaContext) =>
+  z.object({
+    image: image(),
+    alt: z.string(),
+  });
+
+const articles = defineCollection({
+  loader: glob({ base: "./src/data/articles", pattern: "**/*.{md,mdx}" }),
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      author: z.string().optional().default(SITE_AUTHOR),
+      description: z.string(),
+      pubDate: z.coerce.date(),
+      updatedDate: z.coerce.date().optional(),
+      category: z.array(
+        z.enum(
+          CATEGORIES.map((category) => category.name) as [string, ...string[]]
+        )
+      ),
+      heroImage: imageSchema({ image }).optional(),
+    }),
 });
 
-export const collections = { Articles };
+export const collections = { articles };
 export const collectionKey = Object.keys(collections) as CollectionKey[];
 export const collectionDisplayName = ["文章"];
