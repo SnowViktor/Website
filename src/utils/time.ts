0d5e5630773f1b7ee0d897bcd50e16b315daed99
SnowViktor <Snow-Forest@outlook.com>
2025-08-01 01:55:31 +0800

    feat: implement LayoutsTime component for consistent date formatting across articles and navigation

--- src/utils/time.ts
+++ src/utils/time.ts
@@ -2,9 +2,8 @@ export function formatAttributeDate(date: Date): string {
   return date.toISOString();
 }
 
-export function formatContentDate(date: Date, spacing: boolean = true): string {
-  return date
-    .toISOString()
+export function formatContentDate(date: Date, spacing: boolean): string {
+  return formatAttributeDate(date)
     .substring(0, 10)
-    .replace(/-/g, `${spacing ? " / " : "/"}`);
+    .replace(/-/g, spacing ? " / " : "/");
 }
